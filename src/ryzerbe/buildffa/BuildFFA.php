2c5eca23517d054bf67aa4878a53aaaf20dbcc9b
Matze <47496465+Matze997@users.noreply.github.com>
2022-03-12 20:53:33 +0100

    Plugin base

---
+++ src/ryzerbe/buildffa/BuildFFA.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ryzerbe\buildffa;
+
+use pocketmine\plugin\PluginBase;
+
+class BuildFFA extends PluginBase {
+    public function onEnable(): void{
+
+    }
+}
